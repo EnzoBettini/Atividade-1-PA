67e3072b959ee0b3ceb1cc017867c646077d0e2d
Enzo Bettini <enzobettini@hotmail.com>
2026-03-02 20:24:11 -0300

    Criacao de DELETE e GET por ID

--- Atividade Gerenciamento/src/main/java/com/eb/atividadegerenciamento/controllers/EstudanteController.java
+++ Atividade Gerenciamento/src/main/java/com/eb/atividadegerenciamento/controllers/EstudanteController.java
@@ -19,8 +19,18 @@ public class EstudanteController {
         return estudanteService.acharTodos();
     }
 
+    @GetMapping("/{id}")
+    public EstudanteModel getEstudantePorId(@PathVariable Long id) {
+        return estudanteService.acharEstudantePorId(id);
+    }
+
     @PostMapping
     public EstudanteModel  criarEstudante(@RequestBody EstudanteModel estudanteModel) {
         return estudanteService.criarEstudante(estudanteModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarEstudante(@PathVariable Long id) {
+        estudanteService.deletarEstudante(id);
     }
 }
--- Atividade Gerenciamento/src/main/java/com/eb/atividadegerenciamento/services/EstudanteService.java
+++ Atividade Gerenciamento/src/main/java/com/eb/atividadegerenciamento/services/EstudanteService.java
@@ -2,10 +2,12 @@ package com.eb.atividadegerenciamento.services;
 
 import com.eb.atividadegerenciamento.models.EstudanteModel;
 import com.eb.atividadegerenciamento.respositories.EstudanteRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EstudanteService {
@@ -17,7 +19,22 @@ public class EstudanteService {
         return estudanteRepository.findAll();
     }
 
+    public EstudanteModel acharEstudantePorId(Long id) {
+        return estudanteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Estudante não encontrado"));
+    }
+
     public EstudanteModel criarEstudante(EstudanteModel estudanteModel) {
         return estudanteRepository.save(estudanteModel);
+    }
+
+    @Transactional
+    public String deletarEstudante(Long id) {
+        if (estudanteRepository.existsById(id)) {
+            estudanteRepository.deleteById(id);
+            return "Estudante deletado com sucesso";
+        } else {
+            throw new RuntimeException("Estudante com o ID não encontrado");
+        }
     }
 }
